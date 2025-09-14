b67f60123ded58057d278cc4a740b04f2c844cc0
HADJ SASSI Mahdi <mahdihadjsassi1@gmail.com>
2025-09-14 15:45:24 +0200

    update HomePage

--- gui/HomePage/HomePage.hpp
+++ gui/HomePage/HomePage.hpp
@@ -1,13 +1,24 @@
-//
-// Created by mahdihadjsassi on 12/09/2025.
-//
 #ifndef HOMEPAGE_HPP
 #define HOMEPAGE_HPP
 #include "../BasePage.hpp"
 
 class HomePage : public BasePage {
+
+private:
+    sf::RectangleShape playButton_;
+    sf::Text playText_;
+    bool clicked_ = false;
+    mutable sf::FloatRect buttonBounds_{};
+    void layoutButton();
+
+protected:
+    void draw(sf::RenderTarget& target, sf::RenderStates states) const override;
+
 public:
     HomePage();
-    void update() override;
+    void handleEvent(const sf::Event& event) override;
+    bool isFinished() const override;
+    std::unique_ptr<BasePage> next() override;
+
 };
 #endif // HOMEPAGE_HPP
