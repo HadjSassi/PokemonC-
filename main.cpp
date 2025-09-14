a48f074d2bccfb0df0bd848a890944e51e82063f
HADJ SASSI Mahdi <mahdihadjsassi1@gmail.com>
2025-09-14 15:47:30 +0200

    update main

--- main.cpp
+++ main.cpp
@@ -1,33 +1,27 @@
 #include "gui/WelcomePage/WelcomePage.hpp"
-#include "gui/HomePage/HomePage.hpp"
 #include <SFML/Graphics.hpp>
 
-int main() {
-    sf::RenderWindow window(sf::VideoMode(800, 600), "Hello SFML");
 
-    WelcomePage welcomePage;
-    HomePage mainPage;
-    bool showWelcome = true;
+int main() {
+    sf::VideoMode desktop = sf::VideoMode::getDesktopMode();
+    sf::RenderWindow window(desktop, "Pokemon The Game", sf::Style::Default);
+
+    std::unique_ptr<BasePage> state = std::make_unique<WelcomePage>();
 
     while (window.isOpen()) {
         sf::Event event{};
         while (window.pollEvent(event)) {
-            if (event.type == sf::Event::Closed)
-                window.close();
+            if (event.type == sf::Event::Closed) window.close();
+            if (state) state->handleEvent(event);
         }
+
+        if (state && state->isFinished()) {
+            auto next = state->next();
+            if (next) state = std::move(next);
+        }
+
         window.clear();
-
-        if (showWelcome) {
-            welcomePage.update();
-            window.draw(welcomePage);
-            if (welcomePage.isTimeUp()) {
-                showWelcome = false;
-            }
-        } else {
-            mainPage.update();
-            window.draw(mainPage);
-        }
-
+        if (state) window.draw(*state);
         window.display();
     }
     return 0;
