6cdd8b108fb98ca2701ac7356fbe993c582b7c95
HADJ SASSI Mahdi <mahdihadjsassi1@gmail.com>
2025-09-14 15:47:10 +0200

    create a fighting page

---
+++ gui/FightPage/FightPage.cpp
@@ -0,0 +1,7 @@
+#include "FightPage.hpp"
+
+FightPage::FightPage() : BasePage() {
+    text_.setString("Fight");
+    text_.setCharacterSize(48);
+    text_.setFillColor(sf::Color::Red);
+}
---
+++ gui/FightPage/FightPage.hpp
@@ -0,0 +1,13 @@
+#ifndef POKEMON_FIGHTPAGE_HPP
+#define POKEMON_FIGHTPAGE_HPP
+
+
+
+#include "../BasePage.hpp"
+
+class FightPage : public BasePage {
+public:
+    FightPage();
+};
+
+#endif //POKEMON_FIGHTPAGE_HPP
