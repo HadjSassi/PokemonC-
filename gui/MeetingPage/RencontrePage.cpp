c3c88a49eaccec3f29d24551aca34425f3a0a886
HADJ SASSI Mahdi <mahdihadjsassi1@gmail.com>
2025-09-14 15:46:54 +0200

    create a meeting page

---
+++ gui/MeetingPage/RencontrePage.cpp
@@ -0,0 +1,8 @@
+
+#include "RencontrePage.hpp"
+
+RencontrePage::RencontrePage() : BasePage() {
+    text_.setString("Rencontre");
+    text_.setCharacterSize(48);
+    text_.setFillColor(sf::Color::Cyan);
+}
---
+++ gui/MeetingPage/RencontrePage.hpp
@@ -0,0 +1,14 @@
+
+#ifndef RENCONTREPAGE_HPP
+#define RENCONTREPAGE_HPP
+
+
+#include "../BasePage.hpp"
+
+class RencontrePage : public BasePage {
+public:
+    RencontrePage();
+};
+
+
+#endif //RENCONTREPAGE_HPP
