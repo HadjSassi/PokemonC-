183e75c2011cb963c0d2a0f1c114270f5cdc32fa
HADJ SASSI Mahdi <mahdihadjsassi1@gmail.com>
2025-09-14 15:43:36 +0200

    update Welcome Page

--- gui/WelcomePage/WelcomePage.cpp
+++ gui/WelcomePage/WelcomePage.cpp
@@ -1,18 +1,17 @@
-//
-// Created by mahdihadjsassi on 12/09/2025.
-//
 
 #include "WelcomePage.hpp"
+#include "../HomePage/HomePage.hpp"
 
 WelcomePage::WelcomePage() : BasePage() {
     text_.setString("POKEMON the Game!");
     text_.setCharacterSize(48);
     text_.setFillColor(sf::Color::White);
-    text_.setPosition(200, 250);
 }
 
-bool WelcomePage::isTimeUp() const {
+bool WelcomePage::isFinished() const {
     return clock_.getElapsedTime().asSeconds() > 1.0f;
 }
 
-void WelcomePage::update() {}
+std::unique_ptr<BasePage> WelcomePage::next() {
+    return std::make_unique<class HomePage>();
+}
--- gui/WelcomePage/WelcomePage.hpp
+++ gui/WelcomePage/WelcomePage.hpp
@@ -1,17 +1,18 @@
-//
-// Created by mahdihadjsassi on 12/09/2025.
-//
+
 #ifndef WELCOMEPAGE_HPP
 #define WELCOMEPAGE_HPP
 #include "../BasePage.hpp"
 #include <SFML/Graphics.hpp>
 
 class WelcomePage : public BasePage {
-public:
-    WelcomePage();
-    bool isTimeUp() const;
-    void update() override;
+
 private:
     sf::Clock clock_;
+
+public:
+    WelcomePage();
+    bool isFinished() const override;
+    std::unique_ptr<BasePage> next() override;
+
 };
 #endif // WELCOMEPAGE_HPP
