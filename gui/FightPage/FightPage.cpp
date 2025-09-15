b828d31facc58ee479445d3b83a00b49c790edbe
HADJ SASSI Mahdi <mahdihadjsassi1@gmail.com>
2025-09-15 20:03:41 +0200

    show ennemies

--- gui/FightPage/FightPage.cpp
+++ gui/FightPage/FightPage.cpp
@@ -1,7 +1,8 @@
 #include "FightPage.hpp"
-
-#include "FightPage.hpp"
 #include "../HomePage/HomePage.hpp"
+#include <random>
+
+#include "../../entities/headers/Pokedex.hpp"
 
 FightPage::FightPage() : CenteredActionPage() {
     text_.setString("Fight");
@@ -9,11 +10,65 @@ FightPage::FightPage() : CenteredActionPage() {
     text_.setFillColor(sf::Color::Red);
 
     setButtonSize({220.f, 64.f});
-    setButtonColors(sf::Color(178, 34, 34), sf::Color::Black, 2.f); // FireBrick
+    setButtonColors(sf::Color(178, 34, 34), sf::Color::Black, 2.f);
     setButtonText("Continue", 32, sf::Color::White);
+
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<> dis(0, 799);
+
+    randomPokemons_.clear();
+    valueTexts_.resize(6);
+    pokeTextures_.resize(6);
+    pokeSprites_.resize(6);
+
+    for (int i = 0; i < 6; ++i) {
+        auto ptr = Pokedex::getInstance()->getPokemonByIndex(dis(gen));
+        if (ptr) {
+            randomPokemons_.push_back(*ptr);
+            valueTexts_[i].setFont(font_);
+            valueTexts_[i].setString(randomPokemons_.back().getName());
+            valueTexts_[i].setCharacterSize(32);
+            valueTexts_[i].setFillColor(sf::Color::White);
+
+            std::ostringstream oss;
+            oss << "../resources/img/pokemons/" << ptr->getId() << ".png";
+            pokeTextures_[i].loadFromFile(oss.str());
+            pokeSprites_[i].setTexture(pokeTextures_[i]);
+            pokeSprites_[i].setScale(100.f / pokeTextures_[i].getSize().x, 100.f / pokeTextures_[i].getSize().y);
+        }
+    }
 }
 
 unique_ptr<BasePage> FightPage::next() {
-    // Exemple: retour à la HomePage après clic
     return make_unique<class HomePage>();
+}
+
+
+void FightPage::draw(sf::RenderTarget& target, sf::RenderStates states) const {
+    CenteredActionPage::draw(target, states);
+
+    const sf::View& view = target.getView();
+    const sf::Vector2f center = view.getCenter();
+    const sf::Vector2f size = view.getSize();
+
+    float margin = 30.f;
+    float rowHeight = 56.f;
+    float totalHeight = valueTexts_.size() * rowHeight + (valueTexts_.size() - 1) * margin;
+    float startY = center.y - totalHeight / 2.f + 20.f;
+
+    float imgSize = 100.f;
+    float col1X = center.x + size.x / 2.f - 500.f;
+    float col2X = col1X + imgSize + 24.f;
+
+    for (size_t i = 0; i < valueTexts_.size(); ++i) {
+        float y = startY + i * (rowHeight + margin);
+
+        pokeSprites_[i].setPosition(col1X, y);
+        target.draw(pokeSprites_[i], states);
+
+        auto& txt = valueTexts_[i];
+        txt.setPosition(col2X, y + (imgSize - txt.getLocalBounds().height) / 2.f);
+        target.draw(txt, states);
+    }
 }
--- gui/FightPage/FightPage.hpp
+++ gui/FightPage/FightPage.hpp
@@ -1,6 +1,7 @@
 #ifndef POKEMON_FIGHTPAGE_HPP
 #define POKEMON_FIGHTPAGE_HPP
 
+#include "../../entities/headers/Pokemon.hpp"
 #include "../common/CenteredActionPage.hpp"
 
 class FightPage : public CenteredActionPage {
@@ -11,6 +12,12 @@ public:
 
 protected:
     void onButtonClicked() override { CenteredActionPage::onButtonClicked(); }
-};
+    void draw(sf::RenderTarget &target, sf::RenderStates states) const override;
 
+private:
+    std::vector<Pokemon> randomPokemons_;
+    mutable std::vector<sf::Text> valueTexts_;
+    std::vector<sf::Texture> pokeTextures_;
+    mutable std::vector<sf::Sprite> pokeSprites_;
+};
 #endif //POKEMON_FIGHTPAGE_HPP
